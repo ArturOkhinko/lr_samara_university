c54a257c6cab17b730d5dfca7e0b59d643b2e828
ArturOkhinko <okhinko@steadycontrol.com>
2024-10-11 20:31:33 +0400

    not found folder support

--- task5.py
+++ task5.py
@@ -3,7 +3,11 @@ import os
 PATH_TO_NITROGENOUS_BASESS = 'nitrogenous_bases'
 
 def print_all_files_from_folder(path_to_folder):
-    files = os.listdir(path_to_folder)
-    print(files)
-
+    try:
+        files = os.listdir(path_to_folder)
+        print(files)
+    except FileNotFoundError:
+        print("Указанная папка не найдена.")
+        return []
+    
 print_all_files_from_folder(PATH_TO_NITROGENOUS_BASESS)
